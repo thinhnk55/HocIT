7d187e234e0aa525274ac307c5a04492373fd597
thinhnk55 <khanhthinh.45a4@gmail.com>
2021-08-08 20:53:25 +0700

    - Lecture 01: Xin Chao The Gioi

---
+++ src/main/java/bai1/XinChaoTheGioi.java
@@ -0,0 +1,10 @@
+package bai1;
+
+/**
+ * In ra màn hình dòng chữ Xin Chao The Gioi!
+ */
+public class XinChaoTheGioi {
+    public static void main(String[] args){
+        System.out.println("Xin Chao The Gioi!");
+    }
+}
